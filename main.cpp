dbcaf01dba9337f396930ca69f217579e377de81
Nowfal1993 <manowfal07@gmail.com>
2020-11-07 10:42:19 +0000

    added execution time

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include <vector> 
 #include <fstream> 
 #include <inttypes.h>
-#include "BMP_Reader.h"
+#include <ctime> 
 #include <experimental/filesystem>
+#include "BMP_Reader.h"
 
 namespace fs = std::experimental::filesystem;
 
@@ -11,15 +12,15 @@ int main()
 {
 	int w, h, i, j;
 	std::vector<unsigned char> img;
+	// Start measuring time
+	std::clock_t c_start = std::clock();
+
     std::string path("/home/nowfal/Coding_task/BMP_task/bmp_images");
     std::string ext(".bmp");
     for (auto &p : fs::recursive_directory_iterator(path))
     {
         if (p.path().extension() == ext)
         {	
-        	std::cout << "in if"<< "\n"; 
-
-        	//img = read_bmp(p.path().stem().string()+".bmp", w, h); 
         	img = read_bmp(path+"/"+ p.path().stem().string()+".bmp", w, h);
 			char bits[] = "10";
 
@@ -31,34 +32,18 @@ int main()
 			std::cout << "\n"; 
 			}
 
-			std::cout << "Width_o: " << w << "\n"; 
-			std::cout << "Height_o: " << h << "\n";
+			std::cout << "Width: " << w << "\n"; 
+			std::cout << "Height: " << h << "\n";
             std::cout << path+"/"+ p.path().stem().string()+".bmp" << '\n';
-            //std::string filename = p.path().stem().string()+".bmp";
-        	//std::cout << filename << '\n';
+            
         }
 
     }
+    // Stop measuring time and calculate the elapsed time
+	std::clock_t c_end = std::clock();
+
+	long double time_elapsed_ms = 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC;
+	std::cout << "CPU time used: " << time_elapsed_ms << " ms\n";
+
     return 0;
 }
-
-/*int main() { 
-	int w, h, i, j; 
-	std::vector<unsigned char> img = read_bmp("/home/nowfal/Coding_task/BMP_task/bmp_images/1.bmp", w, h); 
-	char bits[] = "10"; 
- 
-	//std::cout << " Width: " << w << "\n"; 
-	//std::cout << "Height: " << h << "\n"; 
- 
-	for (j = 0; j < h; j++) 	{ 
-		for (i = 0; i < w; i++) 
-			std::cout << bits[img[j * w + i]] << " ";  
-		std::cout << "\n"; 
-	}
-
-	std::cout << "Width: " << w << "\n"; 
-	std::cout << "Height: " << h << "\n"; 
-
- 
-	return 0; 
-}*/
